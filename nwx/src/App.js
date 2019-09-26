2095cae5950faf4179b2c84b8adb531652767436
Stephan <stephane1013@hotmail.fr>
2019-09-26 17:40:43 +0200

    restart

--- nwx/src/App.js
+++ nwx/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -8,10 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>
-{' '}
-and save to reload.
-</p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
